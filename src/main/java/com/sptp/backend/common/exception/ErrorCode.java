403317a8443c0a3d302fb3e9844ad0eecb09035e
JunYoung-C <doforme524@gmail.com>
2022-12-27 20:52:21 +0900

    feat: 비밀번호 찾기 관련 예외 정의

--- src/main/java/com/sptp/backend/common/exception/ErrorCode.java
+++ src/main/java/com/sptp/backend/common/exception/ErrorCode.java
@@ -16,7 +16,8 @@ public enum ErrorCode {
     UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "해당 요청은 로그인이 필요합니다."),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
     EXIST_MEMBER(HttpStatus.BAD_REQUEST, "이미 등록된 유저입니다."),
-    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다.");
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다."),
+    NOT_MATCH_EMAIL(HttpStatus.BAD_REQUEST, "이메일이 일치하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
