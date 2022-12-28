686d22f9eec51e59e944b39e850af6095ba8a6fe
Seo Gyu Bum <sgb8170@naver.com>
2022-12-28 15:10:36 +0900

    feat : JWT 토큰 재발급 기능 구현 (#6)
    * feat : JWT 토큰 재발급 기능 구현
    
    * refactor : 코드 리팩토링

--- src/main/java/com/sptp/backend/common/exception/ErrorCode.java
+++ src/main/java/com/sptp/backend/common/exception/ErrorCode.java
@@ -16,7 +16,11 @@ public enum ErrorCode {
     UNAUTHORIZED_MEMBER(HttpStatus.UNAUTHORIZED, "해당 요청은 로그인이 필요합니다."),
     NOT_FOUND_MEMBER(HttpStatus.NOT_FOUND, "해당 유저를 찾을 수 없습니다."),
     EXIST_MEMBER(HttpStatus.BAD_REQUEST, "이미 등록된 유저입니다."),
-    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다.");
+    NOT_MATCH_PASSWORD(HttpStatus.BAD_REQUEST, "패스워드가 일치하지 않습니다."),
+
+    //토큰 예외
+    TOKEN_EXPIRED(HttpStatus.UNAUTHORIZED, "토큰이 만료되었습니다."),
+    TOKEN_INVALID(HttpStatus.NOT_FOUND, "토큰이 유효하지 않습니다.");
 
     private final HttpStatus httpStatus;
     private final String detail;
--- src/main/java/com/sptp/backend/member/web/MemberController.java
+++ src/main/java/com/sptp/backend/member/web/MemberController.java
@@ -1,5 +1,6 @@
 package com.sptp.backend.member.web;
 
+import com.sptp.backend.jwt.service.JwtService;
 import com.sptp.backend.member.web.dto.request.MemberLoginRequestDto;
 import com.sptp.backend.member.web.dto.request.MemberSaveRequestDto;
 import com.sptp.backend.member.web.dto.response.MemberSaveResponseDto;
@@ -9,18 +10,22 @@ import com.sptp.backend.member.service.MemberService;
 import com.sptp.backend.email.service.EmailService;
 import com.sptp.backend.jwt.web.dto.TokenDto;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class MemberController {
 
     private final MemberService memberService;
     private final EmailService emailService;
+    private final JwtService jwtService;
     HashMap<String, String> emailMap = new HashMap<>();
 
     // 회원가입
@@ -49,6 +54,21 @@ public class MemberController {
         TokenResponseDto tokenResponseDto = TokenResponseDto.builder()
                 .accessToken(token.getAccessToken())
                 .refreshToken(token.getRefreshToken())
+                .build();
+
+        return ResponseEntity.status(HttpStatus.OK).body(tokenResponseDto);
+
+    }
+
+    @PostMapping("/members/token")
+    public ResponseEntity<?> refresh(@RequestHeader("refreshToken") String refreshToken){
+
+        //Refresh Token 검증
+        String recreatedAccessToken = jwtService.validateRefreshToken(refreshToken);
+
+        //Access Token 재발급
+        TokenResponseDto tokenResponseDto = TokenResponseDto.builder()
+                .accessToken(recreatedAccessToken)
                 .build();
 
         return ResponseEntity.status(HttpStatus.OK).body(tokenResponseDto);
