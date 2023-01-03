5d2af305a4349f4727942be415b22dd425122487
Choo <choo000407@naver.com>
2023-01-03 20:27:57 +0900

    fix: 이메일 중복 체크 제거, ddl-auto 수정

--- src/main/java/com/sptp/backend/member/service/MemberService.java
+++ src/main/java/com/sptp/backend/member/service/MemberService.java
@@ -154,9 +154,7 @@ public class MemberService {
             .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
 
         if(StringUtils.isNotBlank(dto.getEmail())) {
-            checkDuplicateMemberEmail(dto.getEmail());
             member.setEmail(dto.getEmail());
-        }
         if(StringUtils.isNotBlank(dto.getUsername()))
             member.setUsername(dto.getUsername());
 
