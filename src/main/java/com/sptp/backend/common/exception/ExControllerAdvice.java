1e5049f6f793c58a0d23adb728c598a945c3be35
JunYoung-C <doforme524@gmail.com>
2023-01-03 20:47:39 +0900

    feat: bean validation 예외처리 기능 추가

--- src/main/java/com/sptp/backend/common/exception/ExControllerAdvice.java
+++ src/main/java/com/sptp/backend/common/exception/ExControllerAdvice.java
@@ -1,9 +1,16 @@
 package com.sptp.backend.common.exception;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpMessage;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.http.converter.HttpMessageNotReadableException;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import java.util.Objects;
 
 @Slf4j
 @RestControllerAdvice
@@ -11,10 +18,20 @@ public class ExControllerAdvice {
 
     //일반 에러
     @ExceptionHandler
-    protected ResponseEntity<Object> handleCustomException(CustomException e) {
+    protected ResponseEntity<ErrorResponse> handleCustomException(CustomException e) {
         return ErrorResponse.toResponseEntity(e);
     }
 
+    // api의 bean validation 관련 예외
+    @ExceptionHandler(MethodArgumentNotValidException.class)
+    protected ResponseEntity<ErrorResponse> handleMethodArgumentNotValidException(MethodArgumentNotValidException e) {
+        FieldError fieldError = e.getFieldError();
 
+        if (Objects.isNull(fieldError)) { // 일반적으로는 fieldError가 null이 될 수 없지만, 예외 케이스 처리 용도.
+            return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST);
+        }
+
+        return ErrorResponse.toResponseEntity(HttpStatus.BAD_REQUEST, fieldError);
+    }
 
 }
