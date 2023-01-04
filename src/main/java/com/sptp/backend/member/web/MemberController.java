6842e3e7944151404d8eaa1ef1b8e1e4247e2b9e
JunYoung-C <doforme524@gmail.com>
2023-01-04 11:54:40 +0900

    feat: 비밀번호 확인 검증 기능 추가

--- src/main/java/com/sptp/backend/member/web/MemberController.java
+++ src/main/java/com/sptp/backend/member/web/MemberController.java
@@ -1,6 +1,7 @@
 package com.sptp.backend.member.web;
 
-import com.nimbusds.jwt.JWT;
+import com.sptp.backend.common.exception.CustomException;
+import com.sptp.backend.common.exception.ErrorCode;
 import com.sptp.backend.jwt.service.dto.CustomUserDetails;
 import com.sptp.backend.member.web.dto.request.*;
 import com.sptp.backend.jwt.service.JwtService;
@@ -18,7 +19,6 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.web.bind.annotation.*;
 
 import javax.validation.Valid;
@@ -159,12 +159,22 @@ public class MemberController {
     }
 
     @PatchMapping("/members/password")
-    public ResponseEntity<PasswordChangeRequestDto> changePassword(
-            @RequestBody @Valid PasswordChangeRequestDto passwordChangeRequestDto,
+    public ResponseEntity<PasswordChangeRequest> changePassword(
+            @RequestBody @Valid PasswordChangeRequest passwordChangeRequest,
             @AuthenticationPrincipal CustomUserDetails userDetails) {
-        
-        memberService.changePassword(userDetails.getMember().getId(), passwordChangeRequestDto.getPassword());
+
+        validatePasswordMatching(passwordChangeRequest);
+        memberService.changePassword(userDetails.getMember().getId(), passwordChangeRequest.getPassword());
 
         return ResponseEntity.status(HttpStatus.OK).build();
+    }
+
+    private void validatePasswordMatching(PasswordChangeRequest dto) {
+        String checkPassword = dto.getCheckPassword();
+        String password = dto.getPassword();
+
+        if (!checkPassword.equals(password)) {
+            throw new CustomException(ErrorCode.NOT_MATCH_PASSWORD);
+        }
     }
 }
