8cdc0685db5b0d42fb3312c9c8672091fdeb9b7d
JunYoung-C <doforme524@gmail.com>
2023-01-04 19:57:17 +0900

    feat: memberKeyword 삭제 메서드 호출

--- src/main/java/com/sptp/backend/member/service/MemberService.java
+++ src/main/java/com/sptp/backend/member/service/MemberService.java
@@ -202,7 +202,7 @@ public class MemberService {
     @Transactional
     public void withdrawUser(Long loginMemberId) {
 
-        // TODO 키워드 제거 기능 추가
+        memberKeywordRepository.deleteByMemberId(loginMemberId);
         memberRepository.deleteById(loginMemberId);
     }
 }
--- src/main/java/com/sptp/backend/memberkeyword/repository/MemberKeywordRepository.java
+++ src/main/java/com/sptp/backend/memberkeyword/repository/MemberKeywordRepository.java
@@ -2,5 +2,5 @@ package com.sptp.backend.memberkeyword.repository;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 
-public interface MemberKeywordRepository extends JpaRepository<MemberKeyword, Long> {
+public interface MemberKeywordRepository extends JpaRepository<MemberKeyword, Long>, MemberKeywordCustomRepository {
 }
