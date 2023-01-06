97fab6df64cde07b2d8dc251d4edf05f797dacdf
Choo <choo000407@naver.com>
2023-01-06 15:12:17 +0900

    style: 줄바꿈

--- src/main/java/com/sptp/backend/member/service/MemberService.java
+++ src/main/java/com/sptp/backend/member/service/MemberService.java
@@ -255,11 +255,11 @@ public class MemberService {
                 .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
 
         String imageUrl = awsStorageUrl + findMember.getImage();
-
+        
         if(findMember.isBlankImage()) {
             imageUrl = null;
         }
-        
+
         MemberResponse memberResponse = MemberResponse.builder()
                 .nickname(findMember.getNickname())
                 .userId(findMember.getUserId())
