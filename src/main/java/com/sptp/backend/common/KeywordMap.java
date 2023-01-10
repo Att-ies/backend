eaa192f8eedb40dbd429def6033ca927722e0690
sgb8170@naver.com <sgb8170@naver.com>
2023-01-10 15:09:14 +0900

    feat : 키워드맵 데이터 추가

--- src/main/java/com/sptp/backend/common/KeywordMap.java
+++ src/main/java/com/sptp/backend/common/KeywordMap.java
@@ -17,5 +17,20 @@ public class KeywordMap {
         map.put("미디어아트", 9);
         map.put("풍경화", 10);
         map.put("화려한", 11);
+        map.put("판화", 12);
+        map.put("사진", 13);
+        map.put("소묘", 14);
+        map.put("파스텔", 15);
+        map.put("추상화", 16);
+        map.put("자유로운", 17);
+        map.put("다양한", 18);
+        map.put("개성적인", 19);
+        map.put("율동적인", 20);
+        map.put("편안한", 21);
+        map.put("포근한", 22);
+        map.put("자연적인", 23);
+        map.put("강한", 24);
+        map.put("다이나믹한", 25);
+        map.put("서양적인", 26);
     }
 }
