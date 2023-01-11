0779e03c551b3a964f1dc6a7ab626dfb933e7967
JunYoung-C <doforme524@gmail.com>
2023-01-11 10:24:06 +0900

    feat: chatRoom, Message JPARepository 상속 인터페이스 구현

---
+++ src/main/java/com/sptp/backend/chatroom/repository/ChatRoomRepository.java
@@ -0,0 +1,6 @@
+package com.sptp.backend.chatroom.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomRepository extends JpaRepository<ChatRoom, Long> {
+}
