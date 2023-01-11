ae8127293203925f1b8279fc8e4f5f3e3111ae86
Choo <choo000407@naver.com>
2023-01-12 03:17:43 +0900

    chore: 필요없는 import문 제거

--- src/main/java/com/sptp/backend/member/repository/MemberRepository.java
+++ src/main/java/com/sptp/backend/member/repository/MemberRepository.java
@@ -1,9 +1,7 @@
 package com.sptp.backend.member.repository;
 
-import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
-import java.util.List;
 import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, Long> {
