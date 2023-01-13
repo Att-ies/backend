cd07f12409d142f48858b974f43e54507760d82b
sgb8170@naver.com <sgb8170@naver.com>
2023-01-14 01:47:34 +0900

    test : 회원 정보 조회 테스트 코드 작성

--- src/test/java/com/sptp/backend/member/web/MemberControllerTest.java
+++ src/test/java/com/sptp/backend/member/web/MemberControllerTest.java
@@ -1,5 +1,6 @@
 package com.sptp.backend.member.web;
 import com.sptp.backend.common.BaseControllerTest;
+import com.sptp.backend.member.repository.Member;
 import com.sptp.backend.member.service.MemberService;
 import com.sptp.backend.member.web.dto.request.MemberLoginRequestDto;
 import com.sptp.backend.member.web.dto.request.MemberSaveRequestDto;
@@ -8,6 +9,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.security.test.context.support.WithUserDetails;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.ResultActions;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
@@ -15,6 +17,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 
+import javax.annotation.PostConstruct;
 import java.nio.charset.StandardCharsets;
 import java.util.Arrays;
 import java.util.List;
@@ -35,6 +38,22 @@ class MemberControllerTest extends BaseControllerTest {
 
     @Autowired
     MemberService memberService;
+
+    // 인증 필요한 api 테스트 시 해당 유저 정보 사용
+    @PostConstruct
+    void settingAuthenticatedUser() {
+
+        MemberSaveRequestDto dto = MemberSaveRequestDto.builder()
+                .nickname("nickname2")
+                .userId("test2")
+                .email("test2@gmail.com")
+                .password("test1234")
+                .telephone("01012341234")
+                .keywords(Arrays.asList("사진","파스텔","유화","미디어아트","세련된"))
+                .build();
+
+        memberService.saveUser(dto);
+    }
 
     @Test
     @DisplayName("로컬 회원 가입 테스트")
@@ -104,4 +123,34 @@ class MemberControllerTest extends BaseControllerTest {
         resultActions
                 .andExpect(MockMvcResultMatchers.status().isOk());
     }
+
+    @Test
+    @DisplayName("회원 정보 조회 테스트")
+    @WithUserDetails(value = "test2")
+    public void getUserInfo() throws Exception {
+
+        //when
+        ResultActions resultActions = mockMvc.perform(
+                MockMvcRequestBuilders.get("/members/me")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .characterEncoding(StandardCharsets.UTF_8)
+                        .accept(MediaType.APPLICATION_JSON)
+        )
+                .andDo(MockMvcResultHandlers.print());
+
+        //then
+        resultActions
+                .andExpect(MockMvcResultMatchers.status().isOk())
+                .andExpect(MockMvcResultMatchers.jsonPath("nickname").value("nickname2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("userId").value("test2"))
+                .andExpect(MockMvcResultMatchers.jsonPath("email").value("test2@gmail.com"))
+                .andExpect(MockMvcResultMatchers.jsonPath("telephone").value("01012341234"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.keywords[0]").value("사진"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.keywords[1]").value("파스텔"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.keywords[2]").value("유화"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.keywords[3]").value("미디어아트"))
+                .andExpect(MockMvcResultMatchers.jsonPath("$.keywords[4]").value("세련된"));
+    }
+
+
 }
