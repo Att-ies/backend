c5e50fef619f864d608710ea0a48a3d256266730
Choo <choo000407@naver.com>
2023-01-15 13:00:46 +0900

    feat: static import로 객체 사용

--- src/main/java/com/sptp/backend/member_preferred_artist/repository/MemberPreferredArtistCustomRepositoryImpl.java
+++ src/main/java/com/sptp/backend/member_preferred_artist/repository/MemberPreferredArtistCustomRepositoryImpl.java
@@ -2,8 +2,9 @@ package com.sptp.backend.member_preferred_artist.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sptp.backend.member.repository.Member;
-import com.sptp.backend.member.repository.QMember;
 import lombok.RequiredArgsConstructor;
+import static com.sptp.backend.member_preferred_artist.repository.QMemberPreferredArtist.*;
+import static com.sptp.backend.member.repository.QMember.*;
 
 import java.util.List;
 
@@ -11,15 +12,13 @@ import java.util.List;
 public class MemberPreferredArtistCustomRepositoryImpl implements MemberPreferredArtistCustomRepository{
 
     private final JPAQueryFactory queryFactory;
-    QMemberPreferredArtist preferredArtist = QMemberPreferredArtist.memberPreferredArtist;
-    QMember member = QMember.member;
 
     @Override
     public List<Member> findPreferredArtist(Long memberId) {
         return queryFactory
                 .select(member)
-                .from(member, preferredArtist)
-                .where(member.id.eq(preferredArtist.artist.id), preferredArtist.member.id.eq(memberId))
+                .from(member, memberPreferredArtist)
+                .where(member.id.eq(memberPreferredArtist.artist.id), memberPreferredArtist.member.id.eq(memberId))
                 .fetch();
     }
 }
--- src/main/java/com/sptp/backend/member_preffereed_art_work/repository/MemberPreferredArtWorkCustomRepositoryImpl.java
+++ src/main/java/com/sptp/backend/member_preffereed_art_work/repository/MemberPreferredArtWorkCustomRepositoryImpl.java
@@ -2,8 +2,9 @@ package com.sptp.backend.member_preffereed_art_work.repository;
 
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sptp.backend.art_work.repository.ArtWork;
-import com.sptp.backend.art_work.repository.QArtWork;
 import lombok.RequiredArgsConstructor;
+import static com.sptp.backend.art_work.repository.QArtWork.*;
+import static com.sptp.backend.member_preffereed_art_work.repository.QMemberPreferredArtWork.*;
 
 import java.util.List;
 
@@ -11,15 +12,13 @@ import java.util.List;
 public class MemberPreferredArtWorkCustomRepositoryImpl implements MemberPreferredArtWorkCustomRepository{
 
     private final JPAQueryFactory queryFactory;
-    QMemberPreferredArtWork preferredArtWork = QMemberPreferredArtWork.memberPreferredArtWork;
-    QArtWork artWork = QArtWork.artWork;
-
+    
     @Override
     public List<ArtWork> findPreferredArtWork(Long memberId){
         return queryFactory
                 .select(artWork)
-                .from(artWork, preferredArtWork)
-                .where(artWork.id.eq(preferredArtWork.artWork.id), preferredArtWork.member.id.eq(memberId))
+                .from(artWork, memberPreferredArtWork)
+                .where(artWork.id.eq(memberPreferredArtWork.artWork.id),memberPreferredArtWork.member.id.eq(memberId))
                 .fetch();
     }
 }
