da9c70e45bc8a310fe1fa175954652899d8ef4f6
JunYoung-C <doforme524@gmail.com>
2023-01-17 01:10:49 +0900

    feat: 채팅방 목록 조회 기능 구현

--- src/main/java/com/sptp/backend/message/repository/MessageRepository.java
+++ src/main/java/com/sptp/backend/message/repository/MessageRepository.java
@@ -5,9 +5,14 @@ import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface MessageRepository extends JpaRepository<Message, Long> {
 
     @EntityGraph(attributePaths = {"sender"})
     List<Message> findAllByChatRoomOrderByIdAsc(ChatRoom chatRom);
+
+    Message findFirstByChatRoomOrderByIdDesc(ChatRoom chatRoom);
+
+    Integer countByChatRoomAndIsReadIsFalse(ChatRoom chatRoom);
 }
