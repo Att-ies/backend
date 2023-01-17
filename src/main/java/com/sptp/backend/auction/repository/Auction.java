b93a5eaf272ca490286f65da2710eab48c3cd6f3
JunYoung-C <doforme524@gmail.com>
2023-01-17 19:46:24 +0900

    feat: 경매 엔티티, repository 생성

---
+++ src/main/java/com/sptp/backend/auction/repository/Auction.java
@@ -0,0 +1,26 @@
+package com.sptp.backend.auction.repository;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Auction {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "auction_id")
+    private Long id;
+
+    private Integer turn;
+    private LocalDateTime startDate;
+    private LocalDateTime endDate;
+}
