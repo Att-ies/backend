f48db7d1528044ae2a6123c814ed0b809b30a4b0
Choo <choo000407@naver.com>
2023-01-28 11:55:38 +0900

    feat: ci로 되돌림

--- src/main/java/com/sptp/backend/common/config/PropertyConfig.java
+++ src/main/java/com/sptp/backend/common/config/PropertyConfig.java
@@ -5,8 +5,8 @@ import org.springframework.context.annotation.PropertySource;
 import org.springframework.context.annotation.PropertySources;
 
 @Configuration
-//@PropertySources({
-//        @PropertySource("classpath:env.properties") // env.properties 파일 소스 등록
-//})
+@PropertySources({
+        @PropertySource("classpath:env.properties") // env.properties 파일 소스 등록
+})
 public class PropertyConfig {
 }
