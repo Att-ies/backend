61c58c30f94ce8189a8a1f6a7a4f723795a35d3f
sgb8170@naver.com <sgb8170@naver.com>
2023-01-30 13:41:34 +0900

    feat : 유저의 작품별 최대 입찰가 조회 쿼리 작성

--- src/main/java/com/sptp/backend/bidding/repository/BiddingRepository.java
+++ src/main/java/com/sptp/backend/bidding/repository/BiddingRepository.java
@@ -8,7 +8,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.List;
 import java.util.Optional;
 
-public interface BiddingRepository extends JpaRepository<Bidding, Long> {
+public interface BiddingRepository extends JpaRepository<Bidding, Long>, BiddingCustomRepository {
 
     Optional<Bidding> findByArtWorkAndMember(ArtWork artWork, Member member);
     Optional<Bidding> getFirstByArtWorkOrderByPriceDesc(ArtWork artWork);
