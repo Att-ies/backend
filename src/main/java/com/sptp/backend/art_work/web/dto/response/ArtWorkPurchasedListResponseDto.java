f52206b7103cd9302ea7407cee08a4e8b5e45c52
sgb8170@naver.com <sgb8170@naver.com>
2023-01-30 13:40:17 +0900

    feat : dto 및 컨트롤러 계층 로직 구현

---
+++ src/main/java/com/sptp/backend/art_work/web/dto/response/ArtWorkPurchasedListResponseDto.java
@@ -0,0 +1,66 @@
+package com.sptp.backend.art_work.web.dto.response;
+
+import com.sptp.backend.art_work.repository.ArtWork;
+import lombok.*;
+
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class ArtWorkPurchasedListResponseDto {
+
+    private List<BiddingDto> biddingList;
+    private List<SuccessfulBiddingDto> successfulBiddingList;
+
+    @Data
+    @Builder
+    public static class BiddingDto {
+
+        private Long id;
+        private String mainImage;
+        private Integer turn;
+        private String title;
+        private String artistName;
+        private Long myBiddingPrice;
+        private Long finalBiddingPrice;
+
+        public static BiddingDto from(ArtWork artWork, Long myBiddingPrice, Long finalBiddingPrice) {
+            return BiddingDto.builder()
+                    .id(artWork.getId())
+                    .mainImage(artWork.getMainImage())
+                    .turn(artWork.getAuction().getTurn())
+                    .title(artWork.getTitle())
+                    .artistName(artWork.getMember().getNickname())
+                    .myBiddingPrice(myBiddingPrice)
+                    .finalBiddingPrice(finalBiddingPrice)
+                    .build();
+        }
+    }
+
+    @Data
+    @Builder
+    public static class SuccessfulBiddingDto {
+
+        private Long id;
+        private String mainImage;
+        private Integer turn;
+        private String title;
+        private String artistName;
+        private Long finalBiddingPrice;
+
+        public static SuccessfulBiddingDto from(ArtWork artWork, Long finalBiddingPrice) {
+            return SuccessfulBiddingDto.builder()
+                    .id(artWork.getId())
+                    .mainImage(artWork.getMainImage())
+                    .turn(artWork.getAuction().getTurn())
+                    .title(artWork.getTitle())
+                    .artistName(artWork.getMember().getNickname())
+                    .finalBiddingPrice(finalBiddingPrice)
+                    .build();
+        }
+    }
+
+}
