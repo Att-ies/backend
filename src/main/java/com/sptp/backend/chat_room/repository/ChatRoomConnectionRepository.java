745b3095b11387b9e1c216464f9a95163cdb1bbf
JunYoung-C <doforme524@gmail.com>
2023-02-01 11:54:21 +0900

    feat: 채팅 연결 유지를 위한 엔티티, 클래스 구현

---
+++ src/main/java/com/sptp/backend/chat_room/repository/ChatRoomConnectionRepository.java
@@ -0,0 +1,9 @@
+package com.sptp.backend.chat_room.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface ChatRoomConnectionRepository extends JpaRepository<ChatRoomConnection, Long> {
+
+    boolean existsBySessionId(String sessionId);
+    void deleteBySessionId(String sessionId);
+}
