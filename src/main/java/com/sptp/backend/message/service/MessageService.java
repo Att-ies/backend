bd25b8bd015161c5f7adbe25f1b8ca8e8c88bfd9
JunYoung-C <doforme524@gmail.com>
2023-02-02 21:22:37 +0900

    feat: 이미지 메세지 관련 api 구현

--- src/main/java/com/sptp/backend/message/service/MessageService.java
+++ src/main/java/com/sptp/backend/message/service/MessageService.java
@@ -1,5 +1,6 @@
 package com.sptp.backend.message.service;
 
+import com.sptp.backend.aws.service.FileManager;
 import com.sptp.backend.chat_room.repository.ChatRoom;
 import com.sptp.backend.chat_room_connection.repository.ChatRoomConnectionRepository;
 import com.sptp.backend.chat_room.repository.ChatRoomRepository;
@@ -9,10 +10,13 @@ import com.sptp.backend.member.repository.Member;
 import com.sptp.backend.member.repository.MemberRepository;
 import com.sptp.backend.message.repository.Message;
 import com.sptp.backend.message.repository.MessageRepository;
-import com.sptp.backend.message.web.dto.MessageRequest;
+import com.sptp.backend.message.repository.MessageType;
+import com.sptp.backend.message.web.dto.ImageChatResponse;
+import com.sptp.backend.message.web.dto.MessageResponse;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
+
 
 @Service
 @Slf4j
@@ -23,28 +27,59 @@ public class MessageService {
     private final ChatRoomRepository chatRoomRepository;
     private final ChatRoomConnectionRepository chatRoomConnectionRepository;
     private final MemberRepository memberRepository;
+    private final FileManager fileManager;
 
-    public void saveMessage(MessageRequest messageRequest) {
-
-        Member sender = memberRepository.findById(messageRequest.getSenderId())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
-
-        ChatRoom chatRoom = chatRoomRepository.findById(messageRequest.getChatRoomId())
-                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CHAT_ROOM));
+    public MessageResponse saveMessage(Long senderId, Long chatRoomId, String textMessage) {
 
         Message message = Message.builder()
-                .sender(sender)
-                .chatRoom(chatRoom)
-                .message(messageRequest.getMessage())
-                .isRead(isOtherConnecting(messageRequest)) // 상대방이 접속 상태면 읽음 처리
+                .sender(getMemberOrThrow(senderId))
+                .chatRoom(getChatRoomOrThrow(chatRoomId))
+                .type(MessageType.TEXT.name())
+                .content(textMessage)
+                .isRead(isOtherConnecting(chatRoomId)) // 상대방이 접속 상태면 읽음 처리
                 .build();
 
         messageRepository.save(message);
+
+        return MessageResponse.builder()
+                .chatRoomId(chatRoomId)
+                .message(textMessage)
+                .build();
     }
 
-    private boolean isOtherConnecting(MessageRequest messageRequest) {
+    private boolean isOtherConnecting(Long chatRoomId) {
 
-        int connectionCount = chatRoomConnectionRepository.countByChatRoomId(messageRequest.getChatRoomId());
+        int connectionCount = chatRoomConnectionRepository.countByChatRoomId(chatRoomId);
         return connectionCount == 2; // 발송자와 상대방이 접속한 경우
+    }
+
+    public ImageChatResponse saveImage(Long senderId, Long chatRoomId, String encodedImage) {
+
+        String storeImageFileName = fileManager.storeImageFile(encodedImage);
+
+        Message message = Message.builder()
+                .sender(getMemberOrThrow(senderId))
+                .chatRoom(getChatRoomOrThrow(chatRoomId))
+                .type(MessageType.IMAGE.name())
+                .content(storeImageFileName)
+                .isRead(isOtherConnecting(chatRoomId)) // 상대방이 접속 상태면 읽음 처리
+                .build();
+
+        messageRepository.save(message);
+
+        return ImageChatResponse.builder()
+                .chatRoomId(chatRoomId)
+                .image(fileManager.getFullPath(storeImageFileName))
+                .build();
+    }
+
+    private Member getMemberOrThrow(Long memberId) {
+        return memberRepository.findById(memberId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_MEMBER));
+    }
+
+    private ChatRoom getChatRoomOrThrow(Long chatRoomId) {
+        return chatRoomRepository.findById(chatRoomId)
+                .orElseThrow(() -> new CustomException(ErrorCode.NOT_FOUND_CHAT_ROOM));
     }
 }
