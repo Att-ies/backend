1725be7603b737324041eee621492790b33717fb
Choo <choo000407@naver.com>
2023-02-02 11:43:48 +0900

    style: 자동정렬

--- src/main/java/com/sptp/backend/member/repository/MemberCustomRepositoryImpl.java
+++ src/main/java/com/sptp/backend/member/repository/MemberCustomRepositoryImpl.java
@@ -3,6 +3,7 @@ package com.sptp.backend.member.repository;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import com.sptp.backend.art_work.repository.ArtWork;
 import lombok.RequiredArgsConstructor;
+
 import static com.sptp.backend.art_work.repository.QArtWork.*;
 import static com.sptp.backend.art_work_keyword.repository.QArtWorkKeyword.*;
 
@@ -14,15 +15,15 @@ public class MemberCustomRepositoryImpl implements MemberCustomRepository {
     private final JPAQueryFactory queryFactory;
 
     @Override
-    public List<ArtWork> findCustomizedArtWork(List<Integer> memberKeywordId, Integer page, Integer limit){
+    public List<ArtWork> findCustomizedArtWork(List<Integer> memberKeywordId, Integer page, Integer limit) {
         return queryFactory
                 .select(artWork)
                 .from(artWork, artWorkKeyword)
                 .where(artWork.id.eq(artWorkKeyword.artWork.id), artWorkKeyword.keywordId.in(memberKeywordId))
                 .groupBy(artWork.id)
                 .orderBy(artWork.count().desc(), artWork.createdDate.desc())
-                .limit(limit+1)
-                .offset(limit * (page-1))
+                .limit(limit + 1)
+                .offset(limit * (page - 1))
                 .fetch();
     }
 }
