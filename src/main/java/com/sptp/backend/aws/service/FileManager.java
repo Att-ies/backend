0053abe84da1e71720c0ad474581be8eb6ef1af6
JunYoung-C <doforme524@gmail.com>
2023-02-02 21:11:27 +0900

    feat: 파일 저장을 주관하는 클래스 구현

--- src/main/java/com/sptp/backend/aws/service/FileManager.java
+++ src/main/java/com/sptp/backend/aws/service/FileManager.java
@@ -1,12 +1,49 @@
 package com.sptp.backend.aws.service;
 
-import org.springframework.stereotype.Service;
+import org.apache.logging.log4j.util.Strings;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+import org.springframework.web.multipart.MultipartFile;
 
-@Service
-public class FileService {
+import java.io.File;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.util.Base64;
+import java.util.UUID;
 
-    public String extractExt(String originalFilename) {
-        int pos = originalFilename.lastIndexOf(".");
-        return originalFilename.substring(pos + 1);
+@Component
+public class FileManager {
+
+    private final String storageUrl;
+    private final AwsService awsService;
+
+    public FileManager(@Value("${aws.storage.url}") String storageUrl, AwsService awsService) {
+        this.storageUrl = storageUrl;
+        this.awsService = awsService;
+    }
+
+    public String extractExtension(String originalFileName) {
+        return originalFileName.substring(originalFileName.lastIndexOf(".") + 1);
+    }
+
+    public String getFullPath(String url) {
+        if (Strings.isBlank(url)) {
+            return null;
+        }
+
+        return storageUrl + url;
+    }
+
+    public String storeImageFile(String base64EncodedImage) {
+        byte[] decodedImage = Base64.getDecoder().decode(base64EncodedImage);
+        String storeFileName = UUID.randomUUID() + ".png";
+
+        try {
+            awsService.uploadImage(storeFileName, decodedImage);
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+        return storeFileName;
     }
 }
