a1aaa25c19a3b82f646af5c0cd6629f16c7e57ed
sgb8170@naver.com <sgb8170@naver.com>
2023-02-28 18:04:24 +0900

    fix : 소셜 로그인시 uuid 길이 수정(5자)

--- src/main/java/com/sptp/backend/oauth/service/NaverService.java
+++ src/main/java/com/sptp/backend/oauth/service/NaverService.java
@@ -143,12 +143,10 @@ public class NaverService {
         //DB에 해당 이메일 없을 경우 회원 가입 로직 실행
         if (!memberRepository.existsByEmail(email)) {
 
-            String uuid = UUID.randomUUID().toString().substring(0, 7);
-            String randomNickName = "user_" + uuid;
+            String randomNickName = UUID.randomUUID().toString().substring(0, 5);
 
             while (memberRepository.existsByNickname(randomNickName)) {
-                uuid = UUID.randomUUID().toString().substring(0, 7);
-                randomNickName = "user_" + uuid;
+                randomNickName = UUID.randomUUID().toString().substring(0, 5);
             }
 
             Member member = Member.builder()
